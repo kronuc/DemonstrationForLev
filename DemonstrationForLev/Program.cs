79172b97643209bf653055bc084b12fda3a2c918
kronuc <denerator2@gmail.com>
2023-03-03 20:29:13 +0200

    another changes

--- DemonstrationForLev/Program.cs
+++ DemonstrationForLev/Program.cs
@@ -9,7 +9,7 @@ namespace DemonstrationForLev
             /* 
              * 
              * 
-             * 
+             * якась нова хуйня
              * 
              * 
              * soem comment
